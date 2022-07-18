8b1f22da2e82fbed49c88eaa136ca39e74cf5a67
Deva_kumar.N <ndeva96@gmail.com>
2022-07-17 21:52:17 -0900

    finished

---
+++ styles/scripts/script.js
@@ -0,0 +1,18 @@
+
+function textToAudio () {
+    let msg = document.getElementById('text-to-speech');
+
+    let speech = new SpeechSynthesisUtterance();
+
+    speech.lang = "en-US";
+
+    speech.text = msg;
+
+    speech.volume = 1;
+
+    speech.rate = 1;
+
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
