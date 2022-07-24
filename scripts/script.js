41d1a60934f5e48a118cad4a8aa67194988ebab0
Deva_kumar.N <ndeva96@gmail.com>
2022-07-24 12:35:27 -0900

    created logics

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,14 @@
-let voicelist = responsiveVoice.getVoice();
+let convert = document.querySelector(".convert");
+let textarea = document.querySelector(".textarea");
 
-let allVoices;
+let speech = new SpeechSynthesisUtterance();
 
-for(let i=0; i<voicelist.length; i++){
+convert.addEventListener("click", () => {
 
-    let voice = '<option value = "'+voicelist[i].name+'"> '+voicelist[i].name+'<option>';
-    
-    allVoices = allVoices + voice;
-}
-
-document.getElementById("voice-list").innerHTML = allVoices;
-
-let start = document.getElementById("start");
-
-let pause = document.getElementById("pause");
-
-let resume = document.getElementById("resume");
-
-let canc = document.getElementById("canc");
-
-let rate = document.getElementById("rate");
-
-rate.oninput = function() {
-    document.getElementById("rate-value").innerHTML = rate.value;
-}
-start.onclick = function(){
-    let text = document.getElementById("text").value;
-
-    let lang = document.getElementById("voice-list").value;
-
-    let rateValue = document.getElementById("rate").value;
-
-    responsiveVoice.speak(text,lang,{rate:rateValue});
-}
-
-pause.onclick = function(){
-
-    if (responsiveVoice.isPlaying()){
-
-        responsiveVoice.pause();
-
-        pause.style.display = "none";
-
-        resume.style.display = "inline";
-    }
-}
-resume.onclick = function(){
-
-    if(responsiveVoice.isPlaying()){
-
-        responsiveVoice.resume();
-
-        pause.style.display = "inline";
-
-        resume.style.display = "none";
-
-    }
-}
-
-canc.onclick = function (){
-    
-    responsiveVoice.cancel();
-}
+    speech.text = textarea.value;
+    speech.pitch = 1;
+    speech.volume = 1;
+    speech.lang = "en-US";
+    speech.rate = 1;
+    speechSynthesis.speak(speech);
+});
